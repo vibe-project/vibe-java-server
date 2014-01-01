8eab7249f91f22c71a3407bb7e8813e55848e91e
Donghwan Kim <flowersinthesand@gmail.com>
2014-01-01 20:00:37 +0900

    Update copyright year

--- wes/src/main/java/io/github/flowersinthesand/wes/http/AbstractServerHttpExchange.java
+++ wes/src/main/java/io/github/flowersinthesand/wes/http/AbstractServerHttpExchange.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Donghwan Kim
+ * Copyright 2013-2014 Donghwan Kim
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
